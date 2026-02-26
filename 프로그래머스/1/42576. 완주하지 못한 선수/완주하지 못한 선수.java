def23cd244e9dcb74e17aae6acf56b8610ed1789
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2026-02-27 03:30:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.91 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,14 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {        
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hash = new HashMap<>();
         
-        for(int i = 0; i < completion.length; i++) {
-            if(!participant[i].equals(completion[i]))
-                return participant[i];
-        }
-        return participant[participant.length - 1];
+        for(String p : participant) hash.put(p, hash.getOrDefault(p, 0) + 1);
+        for(String c : completion) hash.put(c, hash.get(c) - 1);
+        
+        for(String s : hash.keySet())
+            if(hash.get(s) != 0) return s;
+        
+        return "";
     }
 }
