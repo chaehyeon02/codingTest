ba8ada9a6cee0ad389aa28a6e10b1a9ab4112add
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2026-02-27 05:10:57 +0900

    [level 2] Title: 더 맵게, Time: 1447.72 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int s : scoville)
+            pq.offer(s);
+        
+        int ans = 0;
+        while(pq.peek() < K) {
+            if(pq.size() < 2) return -1;
+            
+            int first = pq.poll();
+            int second = pq.poll();
+            pq.offer(first + second * 2);
+            ans += 1;
+        }
+        
+        return ans;
+    }
+}
