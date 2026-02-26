fcdd6a8ed9656566182d82147c1a0a108a138ddc
chaehyeon02 <124303720+chaehyeon02@users.noreply.github.com>
2026-02-27 03:23:05 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 321.28 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> hash = new HashMap<>();
+    public String solution(String[] participant, String[] completion) {        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
         
-        for(String p : participant) {
-            if(hash.containsKey(p)) hash.put(p, hash.get(p) + 1);
-            else hash.put(p, 1);
+        for(int i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i]))
+                return participant[i];
         }
-        
-        for(String c : completion) {
-            if(hash.containsKey(c)) {
-                if(hash.get(c) == 1) hash.remove(c);
-                else hash.put(c, hash.get(c) - 1);
-            }
-        }
-        
-        return hash.keySet().iterator().next();
+        return participant[participant.length - 1];
     }
 }
